85e7c96cd659577808714269f64c8f3e80f1c7c1
molubanche <jiangchaodu324@163.com>
2016-07-30 10:09:02 +0800

    测试demo
    测试demo

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by 班车陌路 on 16/7/30.
+//  Copyright © 2016年 班车陌路. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
